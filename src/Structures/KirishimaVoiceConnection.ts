aacd0ea04ef241d79236510f9fe78bbe96569252
KChi <59391215+KagChi@users.noreply.github.com>
2022-03-14 11:39:58 +0700

    fix(KirishimaVoiceConnection): destroy should delete its player

--- src/Structures/KirishimaVoiceConnection.ts
+++ src/Structures/KirishimaVoiceConnection.ts
@@ -64,7 +64,7 @@ export class KirishimaVoiceConnection {
 			op: WebsocketOpEnum.DESTROY,
 			guildId: this.guildId
 		});
-		return this;
+		this.player.kirishima.players?.delete(this.guildId);
 	}
 }
 
