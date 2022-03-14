7c74eb098187425f8ed3f53b765e22111b616801
KChi <59391215+KagChi@users.noreply.github.com>
2022-03-14 11:25:41 +0700

    feat: better connected state

--- src/Structures/KirishimaPlayer.ts
+++ src/Structures/KirishimaPlayer.ts
@@ -1,12 +1,23 @@
-import { isTrack, Kirishima, KirishimaNode, KirishimaPartialTrack, KirishimaPlayerOptions, KirishimaTrack, Structure } from '@kirishima/core';
+import {
+	isPartialTrack,
+	isTrack,
+	Kirishima,
+	KirishimaNode,
+	KirishimaPartialTrack,
+	KirishimaPlayerOptions,
+	KirishimaTrack,
+	Structure
+} from '@kirishima/core';
 import type { LoadTrackResponse } from 'lavalink-api-types';
 
 import { KirishimaQueueTracks } from './KirishimaQueueTracks';
+import { ConnectionState, KirishimaVoiceConnection } from './KirishimaVoiceConnection';
 
 export class KirishimaPlayer extends Structure.get('KirishimaPlayer') {
 	public queue = new KirishimaQueueTracks();
 	public loopType: LoopType = LoopType.None;
 	public playing = false;
+	public connection = new KirishimaVoiceConnection(this);
 
 	public constructor(options: KirishimaPlayerOptions, kirishima: Kirishima, node: KirishimaNode) {
 		super(options, kirishima, node);
@@ -17,11 +28,24 @@ export class KirishimaPlayer extends Structure.get('KirishimaPlayer') {
 		return this;
 	}
 
+	public async connect() {
+		this.connection.state = ConnectionState.Connecting;
+		const player = await super.connect();
+		this.connection.state = ConnectionState.Connected;
+		return player;
+	}
+
+	public async disconnect() {
+		const player = await super.disconnect();
+		this.connection.state = ConnectionState.Disconnected;
+		return player;
+	}
+
 	public resolvePartialTrack(track: KirishimaPartialTrack) {
 		return this.kirishima.resolveTracks(`${track.info.title} - ${track.info.author ? track.info.author : ''}`);
 	}
 
-	public async playTrack(track?: KirishimaTrack | string) {
+	public async playTrack(track?: KirishimaTrack | KirishimaPartialTrack | string) {
 		if (track && isTrack(track)) {
 			return super.playTrack(track);
 		}
@@ -29,16 +53,32 @@ export class KirishimaPlayer extends Structure.get('KirishimaPlayer') {
 		if (this.queue.current && isTrack(this.queue.current)) {
 			return super.playTrack(this.queue.current);
 		}
+
+		if (track && isPartialTrack(track)) {
+			const resolved = await this.resolvePartialTrack(track);
+			if (resolved.tracks.length) {
+				return super.playTrack(resolved.tracks[0].track);
+			}
+		}
+
+		if (this.queue.current && isPartialTrack(this.queue.current)) {
+			const resolved = await this.resolvePartialTrack(this.queue.current);
+			if (resolved.tracks.length) {
+				return super.playTrack(resolved.tracks[0].track);
+			}
+		}
+
 		throw new Error('No track to play');
 	}
 
 	public get connected() {
-		return Boolean(this.voiceServer && this.voiceState);
+		return Boolean(this.voiceServer && this.voiceState?.channel_id && this.connection.state === ConnectionState.Connected);
 	}
 }
 
 declare module '@kirishima/core' {
 	export interface KirishimaPlayer {
+		connection: KirishimaVoiceConnection;
 		playing: boolean;
 		queue: KirishimaQueueTracks;
 		loopType: LoopType;
---
+++ src/Structures/KirishimaVoiceConnection.ts
@@ -0,0 +1,76 @@
+import { createVoiceChannelJoinPayload } from '@kirishima/core';
+import { WebsocketOpEnum } from 'lavalink-api-types';
+import type { KirishimaPlayer } from './KirishimaPlayer';
+
+export class KirishimaVoiceConnection {
+	public state = ConnectionState.Disconnected;
+	public constructor(public player: KirishimaPlayer) {}
+
+	public get region() {
+		return this.player.voiceServer?.endpoint?.split('.').shift()?.replace(/[0-9]/g, '') ?? null;
+	}
+
+	public get channelId() {
+		return this.player.options.channelId;
+	}
+
+	public get textChannelId() {
+		return this.player.options.textChannelId;
+	}
+
+	public get guildId() {
+		return this.player.options.guildId;
+	}
+
+	public get isSelfDeaf() {
+		return (this.player.options.selfDeaf ??= false);
+	}
+
+	public async setSelfDeaf(deaf: boolean) {
+		await this.player.kirishima.options.send(
+			this.player.options,
+			createVoiceChannelJoinPayload(
+				{
+					...this.player.options,
+					selfDeaf: deaf
+				},
+				true
+			)
+		);
+		return this;
+	}
+
+	public async setSelfMute(mute: boolean) {
+		await this.player.kirishima.options.send(
+			this.player.options,
+			createVoiceChannelJoinPayload(
+				{
+					...this.player.options,
+					selfMute: mute
+				},
+				true
+			)
+		);
+		return this;
+	}
+
+	public get isSelfMute() {
+		return (this.player.options.selfMute ??= false);
+	}
+
+	public async destroy() {
+		this.state = ConnectionState.Destroyed;
+		await this.player.node.ws.send({
+			op: WebsocketOpEnum.DESTROY,
+			guildId: this.guildId
+		});
+		return this;
+	}
+}
+
+export enum ConnectionState {
+	Disconnected = 0,
+	Connecting = 1,
+	Connected = 2,
+	Destroyed = 3
+}
